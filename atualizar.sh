6dd4c4d1f7a95571892816d7ebc168843a41fa2b
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2025-05-18 15:11:02 -0300

    atualizando

--- atualizar.sh
+++ atualizar.sh
@@ -43,7 +43,7 @@ for d in ./*; do
 done
 
 cp "README.md" "./../"$repositorio".wiki/Home.md"	
-git add * && git commit -m 'atualizando' && git push
+git pull && git add * && git commit -m 'atualizando' && git push
 
 # se n tiver wiki, favor comentar
 cd "./../"$repositorio".wiki/" && git add * && git commit -m 'atualizando' && git push
